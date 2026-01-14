3b352e61681609041e437397951faa0e663e19df
-=V.I.M.E.R=- <top.michan@mail.ru>
2026-01-14 18:07:07 +0500

    1

--- src/shared/icon/logo.vue
+++ src/shared/icon/logo.vue
@@ -1,22 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const { color = "#fff" } = defineProps<{
+  color?: string;
+}>();
+</script>
 
 <template>
-  <svg width="186" height="180" viewBox="0 0 186 180" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g filter="url(#filter0_d_364_680)">
+  <svg class="h-auto" viewBox="0 0 185 179" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_d_370_689)">
       <path
-        d="M12.3672 134.357L31.6128 88.7925H110.86M12.3672 134.357L28.2165 159.917L49.7264 134.357M12.3672 134.357H49.7264M49.7264 134.357H87.0856L118.784 88.7925H110.86M49.7264 134.357L32.7449 89.9039L20.2919 61.0095M20.2919 61.0095L46.3301 11H79.1609M20.2919 61.0095H99.5387M99.5387 61.0095H130.105L166.332 11L79.1609 11M99.5387 61.0095L79.1609 11M99.5387 61.0095L110.86 88.7925"
-        stroke="#37A5B3"
-        stroke-width="6"
-        shape-rendering="crispEdges"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M17 61L44.5 8H172L131.5 64H104.17L113 86H124.5L89 137.5L51.5 137L28 165L9 134.5L29 88L17 61ZM77.5 14H48.5L25.5 58H95L77.5 14ZM160.5 14H84H83.5L101.5 58H128.5L160.5 14ZM106.5 86L97.5 64H25L34.5 86H106.5ZM85.5 131.5L113 92H37L52 131.5H85.5ZM17 131.5L32 96L45.5 131.5H17ZM28.5 155L18 137.5H43.5L28.5 155Z"
+        fill="#37A5B3"
       />
     </g>
     <defs>
       <filter
-        id="filter0_d_364_680"
+        id="filter0_d_370_689"
         x="0"
         y="0"
-        width="185.21"
-        height="179.015"
+        width="185"
+        height="179"
         filterUnits="userSpaceOnUse"
         color-interpolation-filters="sRGB"
       >
@@ -31,8 +37,8 @@
         <feGaussianBlur stdDeviation="5.5" />
         <feComposite in2="hardAlpha" operator="out" />
         <feColorMatrix type="matrix" values="0 0 0 0 0.215686 0 0 0 0 0.647059 0 0 0 0 0.701961 0 0 0 0.6 0" />
-        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_364_680" />
-        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_364_680" result="shape" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_370_689" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_370_689" result="shape" />
       </filter>
     </defs>
   </svg>
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import logo from "@/shared/icon/logo.vue";
+</script>
 
 <template>
-  <div class="loginIn h-75">login</div>
+  <div class="loginIn h-75 relative">
+    <div class="lo h-max w-max mx-auto absolute top-[-calc(2.1875rem-48px)]"><logo class="w-[100px]" /></div>
+  </div>
 </template>
 
 <style scoped></style>
